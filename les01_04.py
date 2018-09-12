38c4e1dc915be2a1bd2e0b1b1cef9f86eea93509
AnatolyShishkin <shishkinanatoli@gmail.com>
2018-09-12 21:54:08 +0300

    выполнено домашнее задание 4

--- les01_04.py
+++ les01_04.py
@@ -30,6 +30,8 @@ while answer != 'q':
         print(" [2] - выведу информацию о системе")
         print(" [3] - выведу список процессов")
         print(" [4] - продублирую файлы в текущей директории")
+        print(" [5] - дублирую файл, который вы укажите")
+        print(" [6] - удалю все дубликаты в директории")
         
         do = int(input("Укажите номер действия: "))
         
@@ -57,7 +59,28 @@ while answer != 'q':
                 if os.path.isfile(file_list[i]):
                     shutil.copy(file_list[i], file_list[i] + '.dupl')
                 # Чтобы цикл имел возможность завершиться нужно изменять переменную цикла        
-                i += 1    
+                i += 1
+        elif do == 5:
+            print("== Список файлов директории ==")
+            file_list = os.listdir()
+            i = 0
+            while i < len(file_list):
+                print("[", i+1, "] ", file_list[i])
+                i += 1
+            numderFile = int(input("Введите порядковый номер файла для дублирования:"))
+                if os.path.isfile(file_list[numderFile-1]):
+                    shutil.copy(file_list[numderFile-1], file_list[numderFile-1] + '.dupl')
+                    print("Операция дублирования файла ", file_list[numderFile-1], " выполнена")
+        elif do == 6:
+            print("== Удаление дубликатов в директории ==")
+            dirname = input("Укажите имя директории")
+            file_list = os.listdir(dirname)
+            i = 0
+            while i < len(file_list):
+                fullname = os.path.join(dirname, file_list[i]) #формируем полный путь до файла
+                if fullname.endswith('.dupl'):  # проверяем заканчивается ли файл на дупль
+                    os.remove(fullname)         # если это дубль, то он удаляется
+                i += 1
         else:
             pass
        
